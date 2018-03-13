acbc4c229b05d1e4a113b3fb4a43581c03ba1d1b
Erick Ardon <37195436+Elmo524@users.noreply.github.com>
2018-03-13 13:37:26 -0700

    Update CSC-5 Project 2.cpp

--- CSC-5 Project 2.cpp
+++ CSC-5 Project 2.cpp
@@ -13,33 +13,46 @@ int main()
 	float N;
 	float Monthly_Interest_Rate;
 	float Payment;
+	float Amount_Paid_Back;
+	float Interest_Paid;
 	
 	// define L
 	cout << "Enter the loan amount: ";
-	cin >> L;
+	cin >> Loan_Amount;
 	
-	Loan_Amount = L;
+	L = Loan_Amount;
 	
 	// define R
 	cout << "Enter the annual interest rate: ";
-	cin >> R;
+	cin >> Annual_Interest_Rate;
 	
-	Annual_Interest_Rate = R;
+	R = Annual_Interest_Rate;
 	
 	// define N
 	cout << "Enter the number of payments made: ";
-	cin >> N;
+	cin >> Payments_Made;
 	
-	Payments_Made = N;
+	N = Payments_Made;
 	
 	// Define M_I_R
 	Monthly_Interest_Rate = R / 12;
 	
+	// calculate results
+	Payment = (Monthly_Interest_Rate * pow ((1 + Monthly_Interest_Rate), N) / (pow ((1 + Monthly_Interest_Rate), N) - 1))  * L;
+	
+	// Calculate Amount Paid Back
+	Amount_Paid_Back = Payment * N;
+	
+	//Calculate Interest Paid
+	Interest_Paid = Amount_Paid_Back - L;
+	
 	
 	// results
 	cout << "Loan Amount: " << setw(10) << "$" << setprecision(2) << L << endl;
-	cout << "Monthly Interst Rate: " << setw(10) << setprecision(2) << R / 12 << endl;
+	cout << "Monthly Interst Rate: " << setw(10) << setprecision(2) << R / 12 << "%" << endl;
 	cout << "Number of Payments: " << setw(10) << setprecision(2) << N << endl;
-	cout << "Monthly Payment: " << setw(10) << "$" << setprecision(2) << Payment <<
+	cout << "Monthly Payment: " << setw(10) << "$" << setprecision(2) << Payment << endl;
+	cout << "Amount Paid Back: " << setw(10) << "$" << setprecision(2) << Amount_Paid_Back << endl;
+	cout << "Interest Paid: " << setw(10) << "$" << setprecision(2) << Interest_Paid;
 	
 }

87ab317559da65b2f329cbc424324a664d1ca8c1
Erick Ardon <37195436+Elmo524@users.noreply.github.com>
2018-03-15 21:07:01 -0700

    Create CSC-5 Project 3.cpp

---
+++ CSC-5 Project 3.cpp
@@ -0,0 +1,38 @@
+#include <time>
+#include <cstdlib>
+
+rand()
+		srand(time(0))
+int main()
+{
+	int A;
+	int B;
+	int C;
+	int D;
+	int Small;
+	int Big
+	
+	int min = 0;
+	int max = 1600;
+	cin >> max;
+	
+	// Prompt
+	cout << "Enter first integer";
+	cin >> A;
+	
+	cout << "Enter second integer";
+	cin >> B;
+	
+	cout << "Enter second integer";
+	cin << C;
+	
+	cout << "Enter fourth integer";
+	cin >> D;
+	
+	// Calculate
+	if ()
+	
+	// Results
+	cout << "of the numbers " << A <<", " << B << ", " << C << ", and " << D << " ..." << endl;
+	cout << "The smallest is " << Small << ", and the largest is " << Big << endl;
+}

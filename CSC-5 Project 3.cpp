44b367a172f9f784bedce55f98bfab37a5f6c423
Erick Ardon <37195436+Elmo524@users.noreply.github.com>
2018-03-20 20:08:17 -0700

    Update CSC-5 Project 3.cpp

--- CSC-5 Project 3.cpp
+++ CSC-5 Project 3.cpp
@@ -1,8 +1,6 @@
-#include <time>
-#include <cstdlib>
+#include <iostream>
+using namespace std;
 
-rand()
-		srand(time(0))
 int main()
 {
 	int A;
@@ -10,39 +8,46 @@ int main()
 	int C;
 	int D;
 	int Small;
-	int Big
-	
-	int min = 0;
-	int max = 1600;
-	cin >> max;
+	int Big;
 	
 	// Prompt
-	cout << "Enter first integer";
+	cout << "Enter first integer: ";
 	cin >> A;
 	
-	cout << "Enter second integer";
+	cout << "Enter second integer: ";
 	cin >> B;
 	
-	cout << "Enter second integer";
-	cin << C;
+	cout << "Enter second integer: ";
+	cin >> C;
 	
-	cout << "Enter fourth integer";
+	cout << "Enter fourth integer: ";
 	cin >> D;
 	
 	// Calculate for Small
-	if (A < B) && (A < C) && (A < D)
-		A = Small
+	if (A < B && A < C && A < D)
+	 Small = A;
 		
-	if (B < A) && (B < C) && (B < D)
-		B = Small
+	if (B < A && B < C && B < D)
+	 Small = B;
+		
+	if (C < A && C < B && C < D)
+	 Small = C;
+		
+	if (D < A && D < B && D < C)
+	 Small = D;
 	
 	// Calculate for Big
-	if (A > B) && (A > C) && (A > D)
-		A = Big
+	if (A > B && A > C && A > D)
+	 Big = A;
 	
-	if (B > A) && (B > C) && (B > D)
-		B = Big
-	
+	if (B > A && B > C && B > D)
+	 Big = B;
+		
+	if (C > A && C > B && C > D)
+	 Big = C;
+		
+	if (D > A && D > B && D > C)
+	 Big = D;
 	
 	// Results
 	cout << "of the numbers " << A <<", " << B << ", " << C << ", and " << D << " ..." << endl;

c5321e82036dd802baa6f344337262f29eb58cdd
Erick Ardon <37195436+Elmo524@users.noreply.github.com>
2018-04-17 19:34:58 -0700

    Add files via upload

---
+++ April_17_2018_Example_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void a()
+{
+	cout << "Hello from function a()!" << endl;
+}
+
+void b()
+{
+	cout << "Hello form function b()!" << endl;
+}
+
+int main()
+{
+	a();
+	b();
+	
+	return 0;
+}

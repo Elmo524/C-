28a3bbe3de20b3544205a0a9d50fdc3305221d5e
Erick Ardon <37195436+Elmo524@users.noreply.github.com>
2018-05-17 21:04:09 -0700

    Update randoms.cpp

--- randoms.cpp
+++ randoms.cpp
@@ -5,40 +5,50 @@ using namespace std;
 
 int rand_num(int max)
 {
-	int x;
-	
-	unsigned seed = time(0);
-	cout << "seed = " << seed << endl;
-	
-	srand(seed);
-	
-	for(int i=0;i<max;i++)
+	static bool seeded = false;
+	if(!seeded)
 	{
-		cout << rand() << endl;
+		srand(time(0));
+		seeded=true;
 	}
-	
-	return 0;
+	return (rand()%max)+1;
 }
 
-int* fillArray(int x)
+int** fillArray(int x)
 {
-	int *t = new int[x];
+	int **t = new int*[x];
 	for(int i=0;i<x;i++)
-		t[i]=  rand_num(1000);
+		t[i] = new int [x];
+
+	for(int i=0;i<x;i++)
+		for(int j=0; j<x;j++)
+			t[i][j]=rand_num(9);
+	
 	return t;
 }
 
 int main()
 {
+	int x;
 	// Prompt user for size of array
 	cout << "How many random numbers you to you want? ";
+	cin >> x;
 	
 	// call randoms
- 	int *a= fillArray(10);
+ 	int **a= fillArray(x);
 	
-	cout << *a << endl;
-	delete[]a;
-	a= fillArray(10);
+for(int i=0;i<x;i++)
+{
 	
+
+		for(int j=0; j<x;j++)
+		{
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+}
+			for(int i=0;i<x;i++)
+				delete a[i];
+			delete[]a;	
 	return 0;
 }
